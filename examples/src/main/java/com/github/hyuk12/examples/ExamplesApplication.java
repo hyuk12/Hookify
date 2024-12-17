61d5b7c5e6653a841316c4a3b7ddabea4d4d787f
hyuk12 <hotman7025@naver.com>
2024-12-17 14:05:31 +0900

    chore: event type by json test

--- examples/src/main/java/com/github/hyuk12/examples/ExamplesApplication.java
+++ examples/src/main/java/com/github/hyuk12/examples/ExamplesApplication.java
@@ -2,6 +2,7 @@ package com.github.hyuk12.examples;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
 public class ExamplesApplication {
