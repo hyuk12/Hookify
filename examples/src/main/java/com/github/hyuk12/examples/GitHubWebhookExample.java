ff7503fdebef96d69fc66ee90ae810188d07654b
hyuk12 <hotman7025@naver.com>
2024-12-17 16:32:19 +0900

    chore: 이슈 템플릿 외 설정들

--- examples/src/main/java/com/github/hyuk12/examples/GitHubWebhookExample.java
+++ examples/src/main/java/com/github/hyuk12/examples/GitHubWebhookExample.java
@@ -11,7 +11,7 @@ public class GitHubWebhookExample {
 
   public static void main(String[] args) throws IOException {
     // Secret 설정
-    String secret = "ACM+ZoVPpAeRzEVTzYfKlxYChb7DroCT11uTXw23to8=";
+    String secret = "my-secret";
 
     // GitHub WebhookPipeline 생성
     WebhookPipeline pipeline = GitHubWebhookPipeline.create(secret);
