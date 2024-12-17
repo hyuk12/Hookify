d6fbdeebfad99ea038a17d04726f5c234975441d
hyuk12 <hotman7025@naver.com>
2024-12-18 08:47:36 +0900

    chore: println 에서 log 로 변경

--- core/src/main/java/com/hookify/handlers/github/validator/GitHubWebhookValidator.java
+++ core/src/main/java/com/hookify/handlers/github/validator/GitHubWebhookValidator.java
@@ -2,6 +2,7 @@ package com.hookify.handlers.github.validator;
 
 import static com.hookify.util.StringUtils.bytesToHex;
 
+import com.hookify.core.logger.WebhookLogFileService;
 import com.hookify.core.validator.WebhookValidator;
 import com.hookify.handlers.github.processor.GitHubProcessor;
 import java.nio.charset.StandardCharsets;
@@ -12,6 +13,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class GitHubWebhookValidator implements WebhookValidator {
+  private final WebhookLogFileService webhookLogFileService = new WebhookLogFileService();
   private static final String HMAC_ALGORITHM = "HmacSHA256";
   private final String secret;
   private static final Logger logger = LoggerFactory.getLogger(GitHubWebhookValidator.class);
@@ -24,6 +26,7 @@ public class GitHubWebhookValidator implements WebhookValidator {
   public boolean validate(String eventType, String signature, String timestamp, String payload) {
     if (Objects.isNull(signature) || Objects.isNull(payload)) {
       logger.warn("Signature or payload is null");
+      webhookLogFileService.failedLog(eventType, payload);
       return false; // 검증 실패 시 false 반환
     }
 
@@ -34,10 +37,12 @@ public class GitHubWebhookValidator implements WebhookValidator {
       // 입력받은 시그니처와 비교
       boolean isValid = computedHash.equals(signature);
       if (!isValid) {
+        webhookLogFileService.failedLog(eventType, payload);
         logger.warn("Signature validation failed");
       }
       return isValid; // 검증 결과 반환
     } catch (Exception e) {
+      webhookLogFileService.failedLog(eventType, payload);
       logger.error("Signature validation error: {}", e.getMessage());
       return false; // 예외 발생 시 false 반환
     }
