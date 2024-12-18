b8233cb804197f66fc9b3b539102813786b9035e
hyuk12 <hotman7025@naver.com>
2024-12-18 14:42:37 +0900

    chore: discord processor test

--- core/src/main/java/com/hookify/core/logger/WebhookLogFileService.java
+++ core/src/main/java/com/hookify/core/logger/WebhookLogFileService.java
@@ -9,7 +9,6 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
-import jdk.jfr.Event;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
